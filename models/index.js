90a437d82b9e028d5b9cf1f3933634231d0cd490
Christopher Giordano <chrisgcpg@gmail.com>
2024-06-09 15:48:04 -0400

    Created models for User, Post, and Comment. Then linked them through index.js.

---
+++ models/index.js
@@ -0,0 +1,26 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+// User can have many posts and comments
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+// Post can have many comment and is related to user
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+// Comment is related to a user and a post
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
