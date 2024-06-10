3913f8395a1bf73096253ba2d4264f825556b30c
Christopher Giordano <chrisgcpg@gmail.com>
2024-06-09 21:26:28 -0400

    Added some seed files.

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,22 @@
-const Sequalize = require('sequelize');
 require('dotenv').config();
 
-const sequalize = process.env.DB_URL
-? new Sequalize(process.env.DB_URL)
-: new Sequalize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+const Sequelize = require('sequelize');
+console.log(DB_URL);
+console.log(DB_NAME);
+console.log(DB_USER);
+console.log(DB_PASSWORD);
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASSWORD, 
     {
-        host: 'localhost',
-        dialect: 'postgres',
-    }
-)
-module.exports = sequalize;
+      host: 'localhost',
+      dialect: 'postgres',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
+
+module.exports = sequelize;
