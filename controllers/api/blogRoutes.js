fe3c7eb636a84c5b82860995dd9ecd76c389717d
Christopher Giordano <chrisgcpg@gmail.com>
2024-06-10 17:48:15 -0400

    Fixed issue where .env and sequelize were not working properly.

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,2 +1,17 @@
 const router = require('express').Router();
 
+
+const { User } = require('../../models');
+
+router.get('/', async (req, res) => {
+    try {
+        const userData = await User.findAll(req.body);
+        res.status(200).json(userData);
+
+    }
+    catch (err) {
+        res.status(400).json(err);
+    }
+});d
+
+module.exports = router;
