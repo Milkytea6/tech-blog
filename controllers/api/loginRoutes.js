5d1691ef5289b834c53d78fe2eff6b6c779e7099
Christopher Giordano <chrisgcpg@gmail.com>
2024-06-21 19:26:58 -0400

    Added sign-up front and backend.

--- controllers/api/loginRoutes.js
+++ controllers/api/loginRoutes.js
@@ -1,13 +1,22 @@
-const router =require('express').Router();
-
+const router = require('express').Router();
+const { User } = require('../../models');
 router.get('/', (req, res) =>{
     try {
         res.render('login');
-        console.log('Rendered login.')
     } catch (error) {
         console.log('Failed to get login route.')
         res.status(500).json(error);
     }
 });
+router.post('/', async (req, res) => {
+    try {
+        const newUser = await User.create(req.body);
+        console.log(newUser);
+        res.status(200).json({ message: 'User created successfully', newUser });
+    } catch (error) {
+        console.error('Failed to create new user', error);
+        res.status(500).json({message: 'failed to create user'});
+    }
+})
 
 module.exports = router;
